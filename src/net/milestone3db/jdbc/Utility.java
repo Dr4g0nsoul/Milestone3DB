17d25ed2f0ef5c91362a0eda0f1dad9307fb7c57
simon <simon@LAPTOP-3CV9VN4M>
2017-01-13 14:03:32 +0100

    Started Utility class

---
+++ src/net/milestone3db/jdbc/Utility.java
@@ -0,0 +1,23 @@
+package net.milestone3db.jdbc;
+
+public class Utility {
+	public static boolean search(String s, String table){
+		System.out.println(s);
+		return false;
+	}
+	
+	public static boolean insert (String q){
+		System.out.println(q);
+		return false;
+	}
+	
+	public static boolean delete (String q){
+		System.out.println(q);
+		return false;
+	}
+	
+	public static boolean update (String q){
+		System.out.println(q);
+		return false;
+	}
+}
