c1a260f347be3493e81ffe90c4dbd8cc6756f15a
Dr4g0snoul <hoila1994@gmail.com>
2017-01-13 14:04:43 +0100

    JDBCConnector class

--- src/net/milestone3db/jdbc/JDBCConnector.java
+++ src/net/milestone3db/jdbc/JDBCConnector.java
@@ -1,5 +1,24 @@
 package net.milestone3db.jdbc;
 
-public class JDBCConnector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class JDBCConnector {
+	
+	private static final String url = "jdbc:postgresql://localhost/steam";
+	private static final String user = "postgres";
+	private static final String password = "masterkey";
+	private static Connection connection = null;
+	
+	private JDBCConnector() {
+		
+	}
+	
+	public static Connection getInstance() throws SQLException  {
+		if(connection!=null) {
+			connection = DriverManager.getConnection(url, user, password);
+		}
+		return connection;
+	}
 }
