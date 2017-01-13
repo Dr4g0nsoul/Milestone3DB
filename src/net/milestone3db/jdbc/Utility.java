58a1e7f937221046d5b783e50852767aeff0cbd8
Dr4g0snoul <hoila1994@gmail.com>
2017-01-13 14:13:16 +0100

    Utility updated

--- src/net/milestone3db/jdbc/Utility.java
+++ src/net/milestone3db/jdbc/Utility.java
@@ -1,12 +1,23 @@
 package net.milestone3db.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Utility {
-	public static boolean search(String s, String table){
+	public static boolean search(String s, String table) throws SQLException{
+		Connection con = JDBCConnector.getInstance();
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(s);
 		System.out.println(s);
 		return false;
 	}
 	
-	public static boolean insert (String q){
+	public static boolean insert (String q) throws SQLException{
+		Connection con = JDBCConnector.getInstance();
+		Statement stmt = con.createStatement();
+		int addedRows = stmt.executeUpdate(q);
 		System.out.println(q);
 		return false;
 	}
@@ -20,4 +31,5 @@ public class Utility {
 		System.out.println(q);
 		return false;
 	}
+	
 }
