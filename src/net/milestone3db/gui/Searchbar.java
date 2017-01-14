eb60c90f2465a92464558ff77b590e1749d61b1d
Daniel <Daniel@MSI-Daniel>
2017-01-14 14:08:09 +0100

    :/

--- src/net/milestone3db/gui/Searchbar.java
+++ src/net/milestone3db/gui/Searchbar.java
@@ -2,16 +2,22 @@ package net.milestone3db.gui;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.RowFilter;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
+
+import net.milestone3db.jdbc.CustomTableModel;
 
 @SuppressWarnings("serial")
 public class Searchbar extends JPanel {
 	public static JTextField searchField;
+	private static TableRowSorter<CustomTableModel> rowSorter;
 	public Searchbar(String tablename) {
 		setLayout(new FlowLayout());
     	setPreferredSize(new Dimension(1200, 70));
@@ -23,5 +29,20 @@ public class Searchbar extends JPanel {
 		
 		add(searchField);
 		add(searchButton);
+		
+		//rowSorter = new TableRowSorter<CustomTableModel>();
+		TableContentFromDatabase.table.setRowSorter(rowSorter);
+		
+		searchButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String text = searchField.getText();
+                if (text.trim().length() == 0) {
+                    rowSorter.setRowFilter(null);
+                } else {
+                    rowSorter.setRowFilter(RowFilter.regexFilter("(?i)" + text));
+                }
+			}
+		});
 	}
 }
